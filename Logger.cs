f857f4f4eeee79a408c830bdde2e9aa480768af0
Aydın MEMİŞ <aydnmms@outlook.com>
2018-04-07 11:12:56 +0300

    add imagesDownload app

---
+++ Logger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imagesDownload
+{
+   public class Logger
+    {
+        private static string logDirectory = String.Empty;
+
+        ///<summary>
+        ///kurulu dizine log klasörü açıp günlük loglama yapar
+        ///Ayruca mesaj paramteresini göndererk istenilen noktaya loglama yapmamızı sağlar
+        /// </summary>
+        public static void LogMessage(string message, string url)
+        {
+            //günün tarihini aldık
+            DateTime dt = DateTime.Now;
+            //logDirectory kalsörünü oluşturuyoruz
+            if (logDirectory==String.Empty)
+            {
+                SetLogDirectory();
+                if (!Directory.Exists(logDirectory + "Log\\"))
+                {
+                    Directory.CreateDirectory(logDirectory + "Log\\");
+                }
+
+            }
+            //log doyasının adını filePAth değişkenine  yazdırıyoruz 
+            String filePath = logDirectory + "Log\\" + dt.ToString("yyyy-MM-dd") + ".log";
+            // log  dosyası yoksa oluşturuyoruz
+            if (!File.Exists(filePath))
+            {
+                FileStream fs = File.Create(filePath);
+                fs.Close();
+            }
+            try
+            {
+                StreamWriter sw = File.AppendText(filePath);
+                sw.WriteLine(dt.ToString("hh:mm:ss") + "   |   " + url + "   |   "+ message );
+                sw.Flush();
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+        }
+        /// <summary>
+        /// logDirectory değişkenine dosya adını işlenecek dosya adını yazar
+        ///
+        /// </summary>
+        private static void SetLogDirectory()
+        {
+            FileInfo fileInfo = new FileInfo(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            logDirectory = fileInfo.Directory.FullName + "\\";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imagesDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OleDbConnection xlsxConnection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\migros.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES'");
+            int recordCount = 0;
+            try
+            {
+                if (xlsxConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    xlsxConnection.Open();
+                    OleDbCommand sqlCmd = new OleDbCommand("SELECT * FROM [Sayfa1$]", xlsxConnection);
+                    OleDbDataReader xlsxRead = sqlCmd.ExecuteReader();
+                    while (xlsxRead.Read())
+                    {
+                        string barkod = xlsxRead["BARCODE"].ToString();
+                        //string name = xlsxRead["NAME"].ToString();
+                       // string smalImage = xlsxRead["Small Image"].ToString();
+                        string largeImage = xlsxRead["images3cdn"].ToString();
+
+                        try
+                        {
+                            SaveImage(largeImage, barkod, ImageFormat.Png);
+                            recordCount++;
+                            Console.Title ="indirilen image Sayısı:"+recordCount;
+                          //  Console.Clear();
+                        }
+                        catch (Exception hata)
+                        {
+                            Logger.LogMessage(hata.Message.ToString(),largeImage);                          
+                        }
+                       
+                    }
+                    xlsxConnection.Close();
+                    Console.Clear();
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("İşlem Tamamlandı....");
+                    Console.WriteLine("Kaydedilen resim sayısı : {0}", recordCount);
+                    Console.WriteLine("-----------------------------------------");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception hata)
+            {
+
+                Logger.LogMessage(hata.Message.ToString(),null);
+            }
+           
+        }
+
+        private static void SaveImage(string imageUrl, string barkod, ImageFormat format)
+        {
+               using (WebClient client = new WebClient())
+                {
+                    Stream stream = client.OpenRead(imageUrl);
+                    Bitmap bitmap = new Bitmap(stream);
+                    if (bitmap != null)
+                    {
+                        bitmap.Save(@"d:\images3cdn\" + barkod+".jpg",format);
+                   Console.WriteLine("Kaydedilen resmin barkodu :" + barkod);
+                    }
+                    stream.Flush();
+                    stream.Close();
+                    client.Dispose();
+                }
+            }
+        
+    }
+}
